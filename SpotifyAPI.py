7ec1792e098123df21bf18435212623d3cb0255d
vle4904 <le4904@bu.edu>
2024-12-09 20:28:23 -0500

    finished route for starting study sessions

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -38,7 +38,7 @@ def login():
         'response_type': 'code',
         'scope': scope,
         'redirect_uri': REDIRECT_URI,
-        'show_dialog': True     #should remove this line later: forces the user to login in everytime b/c easier to test/debug
+        'show_dialog': True
     }
 
     auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
@@ -68,23 +68,90 @@ def callback():
         session['access_token'] = json_result['access_token']
         session['refresh_token'] = json_result['refresh_token']
         session['expires_at'] = datetime.now().timestamp() + json_result['expires_in']      #Converting 'expires_in' seconds into date
-
-        #Creating variables in session --> tracks' features to update
-        session['total_studysessions'] = 0
-        session['total_songs'] = 0
-        session['avg_acousticness'] = 0
-        session['avg_duration_ms'] = 0
-        session['avg_energy'] = 0
-        session['avg_instrumentalness'] = 0
-        session['avg_liveness'] = 0
-        session['avg_loudness'] = 0
-        session['avg_speechiness'] = 0
-        session['avg_tempo'] = 0
-        session['avg_valence'] = 0
-
-        #Creating variables --> get_recommendations parameters user can adjust
-        session['seed_artists'] = []
-        session['seed_tracks'] = []
-        session['seed_genres'] = []
         
         return redirect('/')
+    
+@app.route('/refresh-token')
+def refresh_token():
+    #Check if there is a refresh_token in session
+    if 'refresh_token' not in session:
+        return redirect('/login')
+    
+    #Check if access_token has expired
+    if datetime.now().timestamp() > session['expires_at']:
+        req_body = {
+            'grant_type': 'refresh_token',
+            'refresh_token': session['refresh_token'],
+            'client_id': client_id,
+            'client_secret': client_secret
+        }
+
+        result = post(TOKEN_URL, data=req_body)
+        new_token_info = result.json()
+
+        #Change old access_token/expires_at for new access_token/expires_at
+        session['access_token'] = new_token_info['access_token']
+        session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in']
+
+        return redirect('/')
+    
+#Record data in a study session --> Input data into a database
+@app.route('/studysession')
+#starts a study session --> argument = duration of study session in minutes
+def studysession():
+    end_studysession = datetime.now().timestamp() + 1 * 60      #duration = 1 minute
+    studysessionList = []
+    session['total_studysessions'] += 1
+
+    while(datetime.now().timestamp() < end_studysession):
+        current_track_info = get_current_track()
+
+        #Error checking: Check if there is a track currently playing
+        if current_track_info != None:
+            if len(studysessionList) == 0:
+                studysessionList.append(current_track_info)
+            elif studysessionList[-1]['name'] != current_track_info['name']:
+                studysessionList.append(current_track_info)
+                print(current_track_info['name'])
+
+        #Retrieve current track every 5 seconds
+        time.sleep(5)
+    
+    #Saving studysessionList as .csv file for Microsoft VS's SQL Database
+    df = pd.DataFrame(studysessionList)
+    
+    df.to_csv(f"studysession{session['total_studysessions']}.csv", index=False)
+
+    return redirect('/')
+
+#Helper function for retreiving current_track
+def get_current_track():
+    url = "https://api.spotify.com/v1/me/player/currently-playing"
+    headers = {
+        'Authorization': f"Bearer {session['access_token']}"
+    }
+
+    result = get(url, headers=headers)
+    #Error Catching: If no song is playing 
+    if result.status_code == 204:
+        return None
+
+    json_result = result.json()     
+
+    #Check if there is an active track playing
+    #Extracting certain data from json dictionary
+    track_id = json_result['item']['id']
+    track_name = json_result['item']['name']
+    artists = json_result['item']['artists']                            #array of Artist Objects
+    artists_names = ', '.join([artist['name'] for artist in artists])    #separating artist names with a comma
+    link = json_result['item']['external_urls']['spotify']
+
+    #Using dictionary to store current track's data
+    current_track_info = {
+        "id": track_id,
+        "name": track_name,
+        "artist": artists_names,
+        "link": link
+    }
+
+    return current_track_info
